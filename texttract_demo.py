7a7023e889a727537a2047270fddd456ac965095
oguzkoroglu <oguzkoroglu@gmail.com>
2023-06-18 04:18:00 +0300

    new photos

--- texttract_demo.py
+++ texttract_demo.py
@@ -2,21 +2,27 @@
 import boto3
 from botocore.exceptions import ClientError
 
+import time
+
 def main():
     session = boto3.Session(profile_name='oguz')
-    textract_client = session.client('textract', region_name='us-east-1')
-    with open("photo_2023-06-02_12-52-19.jpg", "rb") as image:
+    textract_client = session.client('textract', region_name='eu-west-2')
+    with open("photo_2023-06-18_03-22-54.jpg", "rb") as image:
         image_bytes = image.read()
         try:
+            start = time.time()
             response = textract_client.detect_document_text(Document={'Bytes': image_bytes})
+            end = time.time()
+            print(end - start)      
             print("Detected %s blocks.", len(response['Blocks']))
         except ClientError:
             print("Couldn't detect text.")
             raise
         else:
             blocks = response['Blocks']
-            word_blocks = filter(lambda x: x['BlockType'] == 'WORD', blocks)
-            print(word_blocks)
+            word_blocks = (filter(lambda x: x['BlockType'] == 'WORD', blocks))
+            texts = (map(lambda x: x['Text'], word_blocks))
+            print(list(texts))
 
 if __name__ == "__main__":
     main()
